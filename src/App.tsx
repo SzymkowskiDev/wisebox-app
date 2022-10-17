e54c6cd50da1d18b063b03da2c9d63c063a33bb8
kuprelweronika <110784022+kuprelweronika@users.noreply.github.com>
2022-10-17 14:21:08 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> ELOO
+          Edit <code>src/App.tsx</code> ELO
         </p>
       </div>
       <p className="read-the-docs">
