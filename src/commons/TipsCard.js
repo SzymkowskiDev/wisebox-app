b544e791a15ab0682fcb4dad4f59d504cb991663
kuprelweronika <110784022+kuprelweronika@users.noreply.github.com>
2022-10-21 18:39:59 +0200

    test

--- src/commons/TipsCard.js
+++ src/commons/TipsCard.js
@@ -1,8 +1,6 @@
 import "./TipsCard.scss";
-import { Tips } from "./Tips.js";
 
 export const TipsCard = ({ tips }) => {
-  console.log(tips);
   return (
     <div className="card text-center tipsPadding">
       <i className="ci-help_questionmark position-absolute top-0 start-50 translate-middle txt-1 " />
