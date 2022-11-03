916c4c76c094956fc8b9b9b08352495cbc83c0ca
carexo <marcel.mietla3@gmail.com>
2022-11-03 17:29:11 +0100

    change display to bootstrap grid

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -15,73 +15,82 @@ const Dashboard = () => {
   return (
     <>
       <Header user={Users.first} stock={Stockhouses[0]} />
-      <div
-        class="d-flex justify-content-between flex-wrap my-5"
-        style={{ gap: "33px 0px" }}
-      >
-        <NaviagtionBlock
-          title="storage"
-          image={travel}
-          isWide={true}
-          background="linear-gradient(98.18deg, rgba(199, 221, 255, 0.54) 0%, rgba(95, 137, 199, 0.72) 100%)"
-        >
-          10 PRODUCTS ARE OUT OF STOCK!
-        </NaviagtionBlock>
-        <NaviagtionBlock
-          title="templates"
-          image={folder}
-          background="linear-gradient(108.97deg, #EEC799 0%, rgba(238, 219, 169, 0.42) 100%)"
-        >
-          MANAGE TEMPALTES.
-        </NaviagtionBlock>
-        <NaviagtionBlock
-          title="orders"
-          image={bag}
-          reversed={true}
-          background="linear-gradient(71.03deg, #D78EF0 0%, rgba(232, 169, 238, 0.42) 100%)"
-        >
-          MAKE AN ORDER.
-        </NaviagtionBlock>
-        <NaviagtionBlock
-          title="analytics"
-          image={calculator}
-          isWide={true}
-          reversed={true}
-          background="linear-gradient(269.67deg, #99EEB1 0.56%, rgba(169, 238, 213, 0.42) 99.44%)"
-        >
-          GET YOUR REPORT.
-        </NaviagtionBlock>
+      <div class="container-fluid mt-3 mb-4 p-0">
+        <div class="row justify-content-between gx-5">
+          <div class="col col-xxl-7">
+            <NaviagtionBlock
+              title="storage"
+              image={travel}
+              background="linear-gradient(98.18deg, rgba(199, 221, 255, 0.54) 0%, rgba(95, 137, 199, 0.72) 100%)"
+            >
+              10 PRODUCTS ARE OUT OF STOCK!
+            </NaviagtionBlock>
+          </div>
+          <div class="w-100 d-block d-xxl-none"></div>
+          <div class="col col-xxl-5 mt-5 mt-xxl-0">
+            <NaviagtionBlock
+              title="templates"
+              image={folder}
+              background="linear-gradient(108.97deg, #EEC799 0%, rgba(238, 219, 169, 0.42) 100%)"
+            >
+              MANAGE TEMPALTES.
+            </NaviagtionBlock>
+          </div>
+        </div>
+        <div class="row gx-5 mt-xxl-5">
+          <div class="col col-xxl-5 mt-5 mt-xxl-0">
+            <NaviagtionBlock
+              title="orders"
+              image={bag}
+              reversed={true}
+              background="linear-gradient(71.03deg, #D78EF0 0%, rgba(232, 169, 238, 0.42) 100%)"
+            >
+              MAKE AN ORDER.
+            </NaviagtionBlock>
+          </div>
+          <div class="w-100 d-block d-xxl-none"></div>
+          <div class="col col-xxl-7 mt-5 mt-xxl-0">
+            <NaviagtionBlock
+              title="analytics"
+              image={calculator}
+              reversed={true}
+              background="linear-gradient(269.67deg, #99EEB1 0.56%, rgba(169, 238, 213, 0.42) 99.44%)"
+            >
+              GET YOUR REPORT.
+            </NaviagtionBlock>
+          </div>
+        </div>
       </div>
       <div
-        class="d-flex align-items-center ps-5 rounded-bottom rounded-5"
+        class="container rounded-bottom rounded-5 p-4 p-xl-2"
         style={{
           background:
             "linear-gradient(360deg, #FFF6A0 0%, rgba(255, 174, 100, 0.17) 100%)",
-          height: "251px",
         }}
       >
-        <div
-          class="mx-5 text-start"
-          style={{
-            maxWidth: "220px",
-          }}
-        >
-          <h2 class="header-1 mb-5">INCOME</h2>
-          <p class="type-3">General statisctics about your income.</p>
-        </div>
-        <div
-          class="d-flex ms-4"
-          style={{
-            gap: "50px",
-          }}
-        >
-          <IncomeBlock title="today" icon="trending_up" value={2137} />
-          <IncomeBlock title="this week" icon="pie_chart_50" value={6900} />
-          <IncomeBlock
-            title="this month"
-            icon="calendar_calendar"
-            value={42000}
-          />
+        <div class="row align-items-center justify-content-center g-0">
+          <div class="col col-xxl-3 p-xxl-4 mb-4 mb-xxl-0">
+            <div class="text-center text-xxl-start">
+              <h2 class="header-1 mb-3 mb-xxl-5">INCOME</h2>
+              <p class="type-3">General statisctics about your income.</p>
+            </div>
+          </div>
+          <div class="w-100 d-block d-xxl-none"></div>
+          <div class="col col-xl-3 col-xxl-2 mx-4 d-flex justify-content-center d-xl-block">
+            <IncomeBlock title="today" icon="trending_up" value={2137} />
+          </div>
+          <div class="w-100 d-block d-xl-none"></div>
+          <div class="col col-xl-3 col-xxl-2 mx-4 mt-5 mt-xl-0 d-flex justify-content-center d-xl-block">
+            <IncomeBlock title="this week" icon="pie_chart_50" value={6900} />
+          </div>
+          <div class="w-100 d-block d-xl-none"></div>
+          <div class="col col-xl-3 col-xxl-2 mx-4 mt-5 mt-xl-0 d-flex justify-content-center d-xl-block">
+            <IncomeBlock
+              title="this month"
+              icon="calendar_calendar"
+              value={42000}
+            />
+          </div>
         </div>
       </div>
     </>
--- src/pages/dashboard/components/NaviagtionBlock.jsx
+++ src/pages/dashboard/components/NaviagtionBlock.jsx
@@ -1,26 +1,19 @@
 import React from "react";
 
-const NaviagtionBlock = ({
-  children,
-  title,
-  image,
-  isWide,
-  reversed,
-  background,
-}) => {
+const NaviagtionBlock = ({ children, title, image, reversed, background }) => {
+  const direction = reversed ? "e" : "s";
   return (
     <div
-      class={`d-flex ${
+      class={`${
         reversed && "flex-row-reverse"
-      } justify-content-between align-items-end`}
+      } d-flex justify-content-between align-items-end`}
       style={{
-        width: isWide ? "655px" : "480px",
         height: "248px",
         background,
       }}
     >
       <div
-        class={`py-4 p${reversed ? "e" : "s"}-5 text-${
+        class={`py-4 p${direction}-4 p${direction}-lg-5 m${direction}-xl-5 m${direction}-xxl-0 text-${
           reversed ? "end" : "start"
         }`}
         style={{
@@ -30,7 +23,7 @@ const NaviagtionBlock = ({
         <h2 class="header-1">{title.toUpperCase()}</h2>
         <p class="note-3">{children}</p>
         <button
-          class="note-3 py-2 px-4 mt-5 border border-dark border-2"
+          class="note-3 py-2 px-3 mt-4 px-lg-4 mt-lg-5 border border-dark border-2"
           style={{
             backgroundColor: "transparent",
           }}
